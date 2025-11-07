e85a2270aac2ade2f2a72c4253fb88f3245631e2
sagarnanera <sagarnanera30@gmail.com>
2025-11-07 11:07:13 +0530

    chore: Update caching headers to apply to all pages instead of just the pricing page

--- next.config.ts
+++ next.config.ts
@@ -5,22 +5,22 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        // Apply to pricing page
-        source: '/pricing',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, s-maxage=3600, stale-while-revalidate=86400',
-          },
-          {
-            key: 'CDN-Cache-Control',
-            value: 'public, s-maxage=3600',
-          },
-          {
-            key: 'Vary',
-            value: 'x-user-country',
-          },
-        ],
+      // Apply to all pages
+      source: '/:path*',
+      headers: [
+        {
+        key: 'Cache-Control',
+        value: 'public, s-maxage=3600, stale-while-revalidate=86400',
+        },
+        {
+        key: 'CDN-Cache-Control',
+        value: 'public, s-maxage=3600',
+        },
+        {
+        key: 'Vary',
+        value: 'x-user-country',
+        },
+      ],
       },
     ];
   },
